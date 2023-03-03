281ba9dbf4712dc45371ced53c0a9724bd094a7a
Amit Gupta <121490421+amitgupta8@users.noreply.github.com>
2023-03-03 19:16:10 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,2 +1,8 @@
-let yourArray = ['str', 1, true, undefined, null];
-console.log(yourArray.length);
+const yourArray = [
+	[{name:"hello", age:20, female:true}],
+	[{name:"ram", age:20, female:false}],
+	[{name:"shyam", age:20, female:true}],
+	[{name:"dham", age:20, female:true}],
+	[{name:"kam", age:20, female:true}]
+];
+console.log(yourArray);
